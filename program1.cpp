1ad96d626e74ee74f3578cfaa764d9a2e5f9f08f
mamatharn <100469152+mamatharn@users.noreply.github.com>
2022-02-26 16:01:52 +0530

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	//size of character  data type
+	
+	cout<<"Size of char:"<<sizeof(char)<<endl;
+
+	//size of integer data type
+	
+	cout<<"Size of int:"<<sizeof(int)<<endl;
+
+	//size of float data type
+	
+	cout<<"Size of float:"<<sizeof(float)<<endl;
+
+	//size of double data type 
+	
+	cout<<"Size of double:"<<sizeof(double)<<endl;
+
+
+	return 0;
+}
+
+
+
---
+++ program2.cpp
@@ -0,0 +1,24 @@
+//program to find the product of two numbers
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	float x,y,product;
+
+	//read inputs from the user
+	cout<<"Enter two numbers:"<<endl;
+
+	cin>>x>>y;
+
+	product=x*y;
+
+	cout<<"product="<<product<<endl;
+
+
+	return 0;
+}
+
+
+
