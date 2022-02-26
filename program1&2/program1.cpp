35df784b6299c4e5540b36b55e5bed1025ba2abb
gangadharag777 <ask.me.gangadharg@gmail.com>
2022-02-26 16:22:15 +0530

    first

---
+++ program1&2/program1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	//size of character  data type
+	
+	cout<<"Size of char:"<<sizeof(char)<<" byte"<<endl;
+
+	//size of integer data type
+	
+	cout<<"Size of int:"<<sizeof(int)<<" bytes"<<endl;
+
+	//size of float data type
+	
+	cout<<"Size of float:"<<sizeof(float)<<" bytes"<<endl;
+
+	//size of double data type 
+	
+	cout<<"Size of double:"<<sizeof(double)<<" bytes"<<endl;
+
+
+	return 0;
+}
+
+
+
